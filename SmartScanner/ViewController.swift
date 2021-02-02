d87b9ad21ae52b19c3edaad8367cd3d1cda8cfa9
pwndhull07 <er.pwndhull07@gmail.com>
2021-02-02 15:11:56 +0530

    Initial Commit

---
+++ SmartScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartScanner
+//
+//  Created by Pawan Dhull on 02/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
